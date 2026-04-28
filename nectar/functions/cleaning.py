bf82f1ea30366d3dfe3c155993da6b6f47f60a02
Matthew Woods <mawo3010@colorado.edu>
2026-04-28 01:49:22 -0600

    Added merge_clean_feederwatch function for multiple csv file handling. Added 'NoSave' option to clean_feederwatch function.

--- nectar/functions/cleaning.py
+++ nectar/functions/cleaning.py
@@ -26,6 +26,9 @@ def clean_feederwatch(input_csv=None, output_csv=None):
         Path to save cleaned dataset. If None, defaults to
         OUTPUT_DIR/clean_feederwatch.csv
 
+        If  output_csv = 'NoSave', the clean_feederwatch function will return the dataframe
+        without saving to a csv file.
+
     Returns
     -------
     pd.DataFrame
@@ -84,11 +87,76 @@ def clean_feederwatch(input_csv=None, output_csv=None):
     # -------------------------
     # SAVE CLEAN DATA
     # -------------------------
-    fw.to_csv(output_csv, index=False)
+    if output_csv != 'NoSave':
+        fw.to_csv(output_csv, index=False)
 
-    print(f"Saved cleaned data to: {output_csv}")
+        print(f"Saved cleaned data to: {output_csv}")
 
     return fw
+
+
+
+def merge_clean_feederwatch(input_csv_files, output_csv):
+    '''
+    This function is designed to clean multiple RAW Feederwatch data csv files at the same time.
+    Calls clean_feederwatch for each csv without saving, then combines the csv
+    files together before saving them as an output csv with all data combined.
+    
+
+    Inputs:
+    ----------
+    input_csv_files :
+        Accepts a list of paths / str to csv files for RAW feederwatch data.
+
+    
+    outputcsv :
+        Path to save clean dataset of datasets merged together.
+        
+        If outputcsv = 'NoSave', the dataframe will be returned but will not save to a csv
+
+
+    Returns:
+    ----------
+    pd.DataFrame
+        Cleaned FeederWatch dataset containing:
+        - LATITUDE
+        - LONGITUDE
+        - SUBNATIONAL1_CODE
+        - Month, Day, Year
+        - SPECIES_CODE
+        - DATE
+        - DOY
+
+    '''
+    dataframe_list = []
+
+    for file in input_csv_files:
+        df = clean_feederwatch(file, output_csv='NoSave') # Sets df to pd.Dataframe return for clean_feederwatch. Doesn't save
+        dataframe_list.append(df) # Appends to list of cleaned data csv
+
+
+    # Combine all of the csv files in input_csv_files
+    merged_df = pd.concat(dataframe_list, axis=0, ignore_index=False)
+    # Same column names, so I want to stack on axis=0
+    # ignore_index False because column names are important for Feederwatch cleaning
+
+
+
+    # Ensure save path exists
+    Path(output_csv).parent.mkdir(parents=True, exist_ok=True)
+
+
+    # Save cleaned data
+    if output_csv != 'NoSave':
+        merged_df.to_csv(output_csv, index=False)
+
+        print(f"Saved cleaned data to: {output_csv}")
+
+    
+    # Return the merged dataframe
+    return merged_df
+
+
 
 
 # -------------------------
